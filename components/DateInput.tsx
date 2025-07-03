36d8de0bb162e90443f1b057287c65989879b3ad
Pushpal Patil <gauripushp@gmail.com>
2025-07-03 12:41:53 -0400

    took Ben's advice from PR review and broke up the code to make it organized

---
+++ components/DateInput.tsx
@@ -0,0 +1,47 @@
+import DateTimePicker, { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import React, { FC, useState } from "react";
+import { LabeledInput } from "./LabeledInput";
+
+interface Props {
+      label: string;
+      value: string;
+      onDateChange: (s: string) => void;
+}
+
+export const DateInput: FC<Props> = ({ label, value, onDateChange }) => {
+      const [show, setShow] = useState(false);
+      const [internal, setInternal] = useState(new Date());
+
+      const onPicked = (e: DateTimePickerEvent, d?: Date) => {
+            setShow(false);
+            if (e.type === "set" && d) {
+                  setInternal(d);
+                  const display = `${d.getMonth() + 1}`.padStart(2, "0") +
+                        `/${d.getDate()}`.padStart(2, "0") +
+                        `/${d.getFullYear()}`;
+                  onDateChange(display);
+            }
+      };
+
+      return (
+            <>
+                  <LabeledInput
+                        label={label}
+                        value={value}
+                        placeholder="MM/DD/YYYY"
+                        editable={false}
+                        onPress={() => setShow(true)}
+                  />
+                  {show && (
+                        <DateTimePicker
+                              mode="date"
+                              display="spinner"
+                              themeVariant="dark"
+                              textColor="white"
+                              value={internal}
+                              onChange={onPicked}
+                        />
+                  )}
+            </>
+      );
+};
